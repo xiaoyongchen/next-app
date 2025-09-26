6053be8a334e774cf2e0ea3a2d87b217e23168f6
chenxiaoyong <v-chenxiaoyong01@immotors.com>
2025-09-26 14:25:55 +0800

    feat: 设置环境变量

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    // API_URL 按道理是不应该访问的，但是publicRuntimeConfig 在运行时有意安全的访问。打破壁垒。
+    // apiUrl: process.env.API_URL,
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    // dbPassword: process.env.DB_PASSWORD,
+  },
+};
 
 export default nextConfig;
